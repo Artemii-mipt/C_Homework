540d26b96eea4856bf5e677e0b707e87bd0e94d1
Белостоцкий Артемий <belostotskii.ai@phystech.edu>
2021-10-22 01:04:52 +0300

    Исправил ball

--- ДЗ_4/Ball.cpp
+++ ДЗ_4/Ball.cpp
@@ -3,6 +3,13 @@
 
 using namespace std;
 
+double solve(double a, double b, double c) {
+	double D = b * b - 4 * a * c;
+	if (D == 0)
+		return (-b / (2 * a));
+	else
+		return ((-b - std::sqrt(D)) / (2 * a));
+
 Ball::Ball(Point start_pos)
 {
 	position = start_pos;
@@ -17,5 +24,13 @@ void Ball::parameter(double V, double phi)
 void Ball::trajectory(double t)
 {
 	position.x = position.x + V_x * t;
-	position.y = position.y + V_y * t - g * t * t / 2;
+	while (position.y + V_y * t - g * t * t / 2 < 0) {
+		double t_1 = solve(-g / 2, vy, position.y);
+		V_y = V_y - g * t_1;
+		V_y = -V_y;
+		t = t - t1;
+		position.y = 0;
+	}
+	position.y += vy * t - g * t * t / 2;
+	vy -= g * t;
 }
